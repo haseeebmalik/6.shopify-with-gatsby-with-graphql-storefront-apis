8696bea53ccf1e9c58bd6f72288560b4cd1724b6
Haseeb Malik <m.haseebmalik123@gmail.com>
2021-09-30 17:18:55 +0500

    adding some dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,15 @@
+require("dotenv").config();
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-shopify`,
+      options: {
+        password: process.env.SHOPIFY_ADMIN_PASSWORD,
+        storeUrl: process.env.STORE_URL,
+      },
     },
-    plugins: [
-
-    ]
-}
+  ],
+};
